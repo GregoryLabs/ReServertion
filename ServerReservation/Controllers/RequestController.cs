01ef4a98c2619b8f5f49c264a9919201f94e9d73
Josh Gregory <josh@gregorylabs.com>
2019-07-14 18:38:35 -0500

    Scaffolded Server and Request Models
    also added links in layout

---
+++ ServerReservation/Controllers/RequestController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ServerReservation.Data;
+using ServerReservation.Models;
+
+namespace ServerReservation.Controllers
+{
+    public class RequestController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RequestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Request
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Requests.Include(r => r.ApprovedByUser).Include(r => r.RequestedByUser).Include(r => r.Server);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Request/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Requests
+                .Include(r => r.ApprovedByUser)
+                .Include(r => r.RequestedByUser)
+                .Include(r => r.Server)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // GET: Request/Create
+        public IActionResult Create()
+        {
+            ViewData["ApprovedByUserId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["RequestedByUserId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["ServerId"] = new SelectList(_context.Servers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Request/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Timestamp,StartDate,EndDate,IsNewServer,ServerId,RequestedByEmployeeId,RequestedByEmployeeName,RequestedByUserId,RequestJustification,ApprovalStatus,ApprovedByEmployeeId,ApprovedByEmployeeName,ApprovedByUserId,ApprovalComment")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(request);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApprovedByUserId"] = new SelectList(_context.Users, "Id", "Id", request.ApprovedByUserId);
+            ViewData["RequestedByUserId"] = new SelectList(_context.Users, "Id", "Id", request.RequestedByUserId);
+            ViewData["ServerId"] = new SelectList(_context.Servers, "Id", "Id", request.ServerId);
+            return View(request);
+        }
+
+        // GET: Request/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Requests.FindAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApprovedByUserId"] = new SelectList(_context.Users, "Id", "Id", request.ApprovedByUserId);
+            ViewData["RequestedByUserId"] = new SelectList(_context.Users, "Id", "Id", request.RequestedByUserId);
+            ViewData["ServerId"] = new SelectList(_context.Servers, "Id", "Id", request.ServerId);
+            return View(request);
+        }
+
+        // POST: Request/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Timestamp,StartDate,EndDate,IsNewServer,ServerId,RequestedByEmployeeId,RequestedByEmployeeName,RequestedByUserId,RequestJustification,ApprovalStatus,ApprovedByEmployeeId,ApprovedByEmployeeName,ApprovedByUserId,ApprovalComment")] Request request)
+        {
+            if (id != request.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(request);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RequestExists(request.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApprovedByUserId"] = new SelectList(_context.Users, "Id", "Id", request.ApprovedByUserId);
+            ViewData["RequestedByUserId"] = new SelectList(_context.Users, "Id", "Id", request.RequestedByUserId);
+            ViewData["ServerId"] = new SelectList(_context.Servers, "Id", "Id", request.ServerId);
+            return View(request);
+        }
+
+        // GET: Request/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Requests
+                .Include(r => r.ApprovedByUser)
+                .Include(r => r.RequestedByUser)
+                .Include(r => r.Server)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // POST: Request/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var request = await _context.Requests.FindAsync(id);
+            _context.Requests.Remove(request);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RequestExists(int id)
+        {
+            return _context.Requests.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ServerReservation/Data/Migrations/20190714233556_ServerRequest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServerReservation.Data.Migrations
+{
+    public partial class ServerRequest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Servers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Timestamp = table.Column<DateTime>(nullable: false),
+                    Cost = table.Column<double>(nullable: false),
+                    Hostname = table.Column<string>(nullable: true),
+                    HD = table.Column<double>(nullable: true),
+                    HDSize = table.Column<int>(nullable: true),
+                    RAM = table.Column<double>(nullable: true),
+                    RAMSize = table.Column<int>(nullable: true),
+                    CPU = table.Column<string>(nullable: true),
+                    ServerType = table.Column<int>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Note = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Timestamp = table.Column<DateTime>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: true),
+                    EndDate = table.Column<DateTime>(nullable: true),
+                    IsNewServer = table.Column<bool>(nullable: false),
+                    ServerId = table.Column<int>(nullable: true),
+                    RequestedByEmployeeId = table.Column<string>(nullable: true),
+                    RequestedByEmployeeName = table.Column<string>(nullable: true),
+                    RequestedByUserId = table.Column<string>(nullable: true),
+                    RequestJustification = table.Column<string>(nullable: true),
+                    ApprovalStatus = table.Column<int>(nullable: true),
+                    ApprovedByEmployeeId = table.Column<string>(nullable: true),
+                    ApprovedByEmployeeName = table.Column<string>(nullable: true),
+                    ApprovedByUserId = table.Column<string>(nullable: true),
+                    ApprovalComment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requests_AspNetUsers_ApprovedByUserId",
+                        column: x => x.ApprovedByUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Requests_AspNetUsers_RequestedByUserId",
+                        column: x => x.RequestedByUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Requests_Servers_ServerId",
+                        column: x => x.ServerId,
+                        principalTable: "Servers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_ApprovedByUserId",
+                table: "Requests",
+                column: "ApprovedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_RequestedByUserId",
+                table: "Requests",
+                column: "RequestedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_ServerId",
+                table: "Requests",
+                column: "ServerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "Servers");
+        }
+    }
+}
